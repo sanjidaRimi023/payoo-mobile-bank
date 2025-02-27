b1392a9d9bdb164e8967f8a37c780847b74fb014
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-02-27 23:07:09 +0600

    added pay section

--- js/cashout.js
+++ js/cashout.js
@@ -1,17 +1,4 @@
-document.getElementById('cashout-btn')
-.addEventListener('click', function(event){
+document.getElementById("cashout-btn")
+.addEventListener("click", function(event){
     event.preventDefault();
-    const agentNumber = document.getElementById('agent-number').value;
-    const amount = getInputValue
-    if(agentNumber.length === 11){
-        if(agentNumber && amount){
-
-        }
-        else{
-            
-        }
-    }else{
-        alert('Enter valid Agent number');
-    }
-
 })
--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,46 @@
-document.getElementById('addmoney').addEventListener('click', function(){
+document.getElementById('addmoney').style.display = "block";
+document.getElementById('cashout').style.display = "none";
+document.getElementById('transfer').style.display = "none";
+document.getElementById('bonus').style.display = "none";
+document.getElementById('bill-pay').style.display = "none";
+
+
+document.getElementById('add-money-box').addEventListener('click', function(){
     document.getElementById('addmoney').style.display = "block";
     document.getElementById('cashout').style.display = "none";
+    document.getElementById('transfer').style.display = "none";
+    document.getElementById('bonus').style.display = "none";
+    document.getElementById('bill-pay').style.display = "none";
 });
-document.getElementById('cashout').addEventListener('click', function(){
-    document.getElementById('addmoney').style.display = "none";
+document.getElementById('cash-Out').addEventListener('click', function(){
     document.getElementById('cashout').style.display = "block";
+    document.getElementById('addmoney').style.display = "none";
+    document.getElementById('bonus').style.display = "none";
+    document.getElementById('bill-pay').style.display = "none";
+    document.getElementById('transfer').style.display = "none";
 });
+document.getElementById('transfer-box').addEventListener('click', function(){
+    document.getElementById('transfer').style.display = "block";
+    document.getElementById('addmoney').style.display = "none";
+    document.getElementById('bonus').style.display = "none";
+    document.getElementById('bill-pay').style.display = "none";
+    document.getElementById('cashout').style.display = "none";
+});
+document.getElementById('bill-box').addEventListener('click', function(){
+    document.getElementById('bill-pay').style.display = "block";
+    document.getElementById('addmoney').style.display = "none";
+    document.getElementById('bonus').style.display = "none";
+    document.getElementById('transfer').style.display = "none";
+    document.getElementById('cashout').style.display = "none";
+});
+document.getElementById('get-bonus').addEventListener('click', function(){
+    document.getElementById('bonus').style.display = "block";
+    document.getElementById('addmoney').style.display = "none";
+    document.getElementById('bill-pay').style.display = "none";
+    document.getElementById('transfer').style.display = "none";
+    document.getElementById('cashout').style.display = "none";
+});
+document.getElementById('Logout-btn').addEventListener('click', function(){
+    window.location.href = "./index.html"
+})
+
